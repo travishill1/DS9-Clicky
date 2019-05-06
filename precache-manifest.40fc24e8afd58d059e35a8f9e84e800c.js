c0ad1ba8b8f0eabd58c51758bee5ee9083f008e3
travishill1 <travishill1@gmail.com>
2019-05-06 15:48:06 -0500

    Updates

---
+++ precache-manifest.40fc24e8afd58d059e35a8f9e84e800c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17684c38df33d512addd6226dde14a9",
+    "url": "/DS9-Clicky/index.html"
+  },
+  {
+    "revision": "becad714227ca74cdbef",
+    "url": "/DS9-Clicky/static/css/main.c705ac07.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/DS9-Clicky/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "becad714227ca74cdbef",
+    "url": "/DS9-Clicky/static/js/main.266f55ae.chunk.js"
+  },
+  {
+    "revision": "c5929dbdd94fda7ca24e",
+    "url": "/DS9-Clicky/static/js/runtime~main.bfb222f6.js"
+  }
+]);
